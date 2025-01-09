c832abde0726e5ecbf125ef460adef66ca9b21b0
devhan-hub <hanabduman@gmail.com>
2025-01-09 16:21:49 +0300

    adding header

---
+++ components/Header.js
@@ -0,0 +1,11 @@
+const Header = () => {
+  return (
+    <div className='w-full min-h-16 p-2  shadow-lg bg-red-500 relative'>
+       <p className='text-lg font-bold text-center '>AAU Campus Navigator</p>
+      
+     
+    </div>
+  )
+}
+
+export default Header
